b1c3fae92a53dcaf161221d9e625a6703c25f21f
Dan H <spag1024@hotmail.com>
2019-07-19 05:48:33 -0500

    created a failing test

---
+++ test/request-time.spec.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+const requestTime = require('../requestTime');
+
+
+describe('Testing requestTime middleware', function(){
+    it('Should add a timestamp `requestTime` prop to req', function(){
+        const req = {};
+        requestTime(req);
+
+        assert.ok(req.requestTime > 0);
+    });
+});
