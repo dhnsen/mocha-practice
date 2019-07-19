90ba806ce559acb9088e7eb48ad6dbb269abd020
Dan H <spag1024@hotmail.com>
2019-07-19 05:50:57 -0500

    made the test pass

--- test/request-time.spec.js
+++ test/request-time.spec.js
@@ -5,7 +5,7 @@ const requestTime = require('../requestTime');
 describe('Testing requestTime middleware', function(){
     it('Should add a timestamp `requestTime` prop to req', function(){
         const req = {};
-        requestTime(req);
+        requestTime(req, null, () => {});
 
         assert.ok(req.requestTime > 0);
     });
